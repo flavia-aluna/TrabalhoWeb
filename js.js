5fd88878db5f96c20749debbc2a75aeafcb99107
flavia-aluna <flavia.mvieira@souunit.com.br>
2023-11-05 23:43:20 -0300

    Adição dos efeitos nas imagens do corpo

--- js.js
+++ js.js
@@ -7,7 +7,6 @@ function oqueeIA() {
     let ia = window.document.getElementById("oqueeIA")
     ia.style.transform = 'translateY(5px)';
 }
-
 function tipos() {
     let tip = window.document.getElementById("tipos")
     tip.style.transform = 'translateY(5px)';
@@ -23,18 +22,50 @@ function plataforma() {
     plat.style.transform = 'translateY(5px)';
 }
 
+/*Inicio do código para o relógio*/
 function atualizarRelogio() {
-    const relogioElement = document.getElementById("relogio");
-    const dataAtual = new Date();
+const relogioElement = document.getElementById("relogio");
+const dataAtual = new Date();
 
-    const horas = dataAtual.getHours().toString().padStart(2, "0");
-    const minutos = dataAtual.getMinutes().toString().padStart(2, "0");
-    const segundos = dataAtual.getSeconds().toString().padStart(2, "0");
+const horas = dataAtual.getHours().toString().padStart(2, "0");
+const minutos = dataAtual.getMinutes().toString().padStart(2, "0");
+const segundos = dataAtual.getSeconds().toString().padStart(2, "0");
 
-    relogioElement.textContent = `Agora são: ${horas}:${minutos}:${segundos}`;
-  }
+relogioElement.textContent = `Agora são: ${horas}:${minutos}:${segundos}`;
+}
 
-  setInterval(atualizarRelogio, 1000); // Atualiza a cada segundo
+setInterval(atualizarRelogio, 1000); // Atualiza a cada segundo
+/*fim do código para o relógio*/
 
+/*Inicio do código para o efeito das imagens do corpo*/
+const zoomImage = document.getElementById("fundo3dIA");
 
+zoomImage.addEventListener("mouseover", () => {
+zoomImage.classList.add("zoomed");
+});
+
+zoomImage.addEventListener("mouseout", () => {
+zoomImage.classList.remove("zoomed");
+});
+
+const image = document.getElementById("desenhoIA");
+
+image.addEventListener("mouseover", () => {
+image.classList.add("zoomed");
+});
+
+image.addEventListener("mouseout", () => {
+image.classList.remove("zoomed");
+});
+
+const brazil = document.getElementById("abeinfobrasil");
+
+brazil.addEventListener("mouseover", () => {
+brazil.classList.add("zoomed");
+});
+
+brazil.addEventListener("mouseout", () => {
+brazil.classList.remove("zoomed");
+});
+/*Fim do código para o efeito das imagens do corpo*/
 
